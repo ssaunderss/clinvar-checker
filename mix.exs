3bdf4fe02ed52c0723631a0f8e11a056172f8846
Austin Saunders <42239860+ssaunderss@users.noreply.github.com>
2025-02-11 19:17:46 -0600

    updates logic to no longer return heterozygous variants with novel alts (#4)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ClinvarChecker.MixProject do
   def project do
     [
       app: :clinvar_checker,
-      version: "1.0.1",
+      version: "1.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
