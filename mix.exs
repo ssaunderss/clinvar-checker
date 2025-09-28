435ae0450ba972418e29babac57dac41e3b2735c
Austin Saunders <austinsaunders@me.com>
2025-09-28 11:12:00 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ClinvarChecker.MixProject do
   def project do
     [
       app: :clinvar_checker,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
